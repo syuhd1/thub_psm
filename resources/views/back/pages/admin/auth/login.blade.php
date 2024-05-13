af3800d3e147bfd1f8c40c32a7dc2f588e882d97
Nurul Syuhada Binti Othman <nurulsyhda22@gmail.com>
2024-05-14 01:16:42 +0800

    add a part of auth early

---
+++ resources/views/back/pages/admin/auth/login.blade.php
@@ -0,0 +1,5 @@
+@extends('back.layout.auth-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Admin Login');
+@section('content')
+    content here
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -2,8 +2,22 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
+Route::prefix('admin')->name('admin.')->group(function(){
+
+    Route::middleware(['guest:admin'])->group(function(){
+        Route::view('/login', 'back.pages.admin.auth.login')->name('login');
+    });
+
+    Route::middleware(['auth:admin'])->group(function(){
+        //paused 13/5/24 - add here later vid #7 timestamp 1:52
+        Route::view('/home', 'back.pages.admin.home')->name('home');
+    });
+});
+
+/*
 Route::get('/admin', function(){
     return 'hello admin';
 }
-);
+); */
